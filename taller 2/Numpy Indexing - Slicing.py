b2a1e108a2ddac33ff36014b7e95d11afed51fed
Álvaro Alejandro Zarabanda Gutiérrez <aazarabandag@correo.udistrital.edu.co>
2021-11-26 18:10:16 -0500

    se modifica a funcion cambiarBloques

--- taller 2/Numpy Indexing - Slicing.py
+++ taller 2/Numpy Indexing - Slicing.py
@@ -1,26 +1,34 @@
 import numpy as np
 
 def calcularSlice(bloque):
-  z, y, x = bloque//9*3, (bloque//3)%3*3, bloque%3*3 
-  return z,y,x
+    z, y, x = bloque//9*3, (bloque//3)%3*3, bloque%3*3 
+    return tuple(slice(i, i+3) for i in (z, y, x))
 
-def sliceBlock(a,bloque):
-    z,y,x=calcularSlice(bloque)           
-    return a[z:z+3 , y:y+3 , x:x+3]
+def cambiarBloques(a,bloque1,bloque2):
+    z1, y1, x1 = calcularSlice(bloque1)
+    z2, y2, x2 = calcularSlice(bloque2)
+    temp = a[z1 , y1 , x1].copy()
+    a[z1 , y1 , x1] = a[z2 , y2 , x2]
+    a[z2 , y2 , x2] = temp
+    return a    
 
 if __name__ == "__main__":
     a = np.arange(729).reshape(9,9,9)
-    print("Array Completo:")
-    print(a)
-    while(True):
-        print("Digite -1 si quiere salir")
-        bloque = int(input("Digite el indice del cubo: "))
-        print("-------------------------------")
-        if(bloque == -1):
+    print("Intercambio de posiciones de subArrays 3x3x3")
+    print("Array Original:","\n")
+    print(a,"\n")
+    while(True):        
+        bloque1 = int(input("Digite el indice del primer subArray: "))
+        bloque2 = int(input("Digite el indice del segundo subArray: "))
+        cambiarBloques(a,bloque1,bloque2)
+        print("Se han intercambiado las posiciones")
+        print("Array Nuevo:","\n\n",a,"\n")
+        salir = input("Digite -1 si quiere salir, luego enter \nO solo presione enter para continuar:  ")
+        if(salir == "-1"):
             break
-        else:
-            print(sliceBlock(a,bloque),"\n")
-            print(calcularSlice(bloque))
+        print("-------------------------------")
+        
+            
 
    
   
